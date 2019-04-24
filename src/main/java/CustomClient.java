6a21b6abf376a3d976d75e1e787544139a8e3f8a
Bluemi <bruno.schilling@protonmail.ch>
2019-04-24 15:34:48 +0200

    starts 4 random clients now

--- src/main/java/CustomClient.java
+++ src/main/java/CustomClient.java
@@ -1,17 +1,28 @@
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import lenz.htw.sawhian.Move;
-import lenz.htw.sawhian.Server;
+// import lenz.htw.sawhian.Server;
 import lenz.htw.sawhian.net.NetworkClient;
 
 public class CustomClient {
     public static void main(String[] args) throws IOException {
-        Server server = new Server();
-        BufferedImage logo = ImageIO.read(new File("meinBild.png"));
-        NetworkClient client = new NetworkClient("ip adresse server (null = localhost)", "CLIENT 0", logo);
+        new Thread(() -> { runRandomClient("Client 0", "test1.png"); }).start();
+        new Thread(() -> { runRandomClient("Client 1", "test1.png"); }).start();
+        new Thread(() -> { runRandomClient("Client 2", "test1.png"); }).start();
+        new Thread(() -> { runRandomClient("Client 3", "test1.png"); }).start();
+    }
+
+    private static void runRandomClient(final String clientName, final String imagePath) {
+        BufferedImage logo;
+        try {
+            logo = ImageIO.read(CustomClient.class.getResourceAsStream(imagePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        NetworkClient client = new NetworkClient(null, clientName, logo);
 
         client.getMyPlayerNumber();
         client.getTimeLimitInSeconds();
