2f6bcf72ae147cda23e3d4e3fe52847a5f315a3e
Philipp <philippboensch@gmail.com>
2019-05-14 10:57:53 +0200

    simple minimax implemented

--- src/main/java/DecisionRuleAlgorithm.java
+++ src/main/java/DecisionRuleAlgorithm.java
@@ -1,18 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DecisionRuleAlgorithm {
 
     private int initialDepth;
     private int playerNumber;
     private long selectedMove = 0L;
 
-    public int minmax(GameState state, int depth) {
-        var moves = state.getNextPossibleMoves(((byte) state.getCurrentPlayer()));
+    public float minmax(GameState state, int depth) {
+        var moves = state.getNextPossibleMoves();
         if (depth == 0 || moves == 0 || state.isGameOver())
             return state.calculateScore(playerNumber);
 
-        var topScore = playerNumber == state.getCurrentPlayer() ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        var topScore = playerNumber == state.getCurrentPlayer() ? -Float.MAX_VALUE : Float.MAX_VALUE;
 
         for (var move : new Configurations.TokenPositions(moves)) {
-            var newState = state.createStateFromMove(currentPlayer, move);
+            var newState = state.createStateFromMove(move);
             var score = minmax(newState, depth - 1);
 
             if (playerNumber == state.getCurrentPlayer()) {
@@ -34,12 +38,28 @@ public class DecisionRuleAlgorithm {
     }
 
     public static void main(String[] args) {
+        Random randomGenerator = new Random();
         var dra = new DecisionRuleAlgorithm();
         dra.playerNumber = 0;
-        dra.initialDepth = 3;
+        dra.initialDepth = 5;
         var state = GameState.newEmptyGameState();
-        var score = dra.minmax(state, dra.initialDepth);
-        System.out.println(dra.selectedMove);
+
+        while (true) {
+            var score = dra.minmax(state, dra.initialDepth);
+            state = state.createStateFromMove(dra.selectedMove);
+            System.out.println(state);
+
+            for (var i = 0; i < 3; i++) {
+                var moves = new ArrayList<Long>();
+                for (var move : new Configurations.TokenPositions(state.getNextPossibleMoves()))
+                    moves.add(move);
+
+                int index = randomGenerator.nextInt(moves.size());
+                state = state.createStateFromMove(moves.get(index));
+                System.out.println(state);
+            }
+        }
+
 
     }
 }
