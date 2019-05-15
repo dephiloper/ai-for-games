48ae6d4c5334e7a0041edae43721b6a56f5b823e
Bluemi <bruno.schilling@protonmail.ch>
2019-05-15 16:26:30 +0200

    fixed something

--- src/main/java/de/htw/aiforgames/Client.java
+++ src/main/java/de/htw/aiforgames/Client.java
@@ -48,7 +48,7 @@ public class Client {
                 var move = this.decisionAlgorithm.getNextMove(this.gameState);
                 serverMove = createServerMove(this.playerNumber, move);
                 this.networkClient.sendMove(serverMove);
-                this.gameState = this.gameState.checkedCreateStateFromMove(move);
+                // this.gameState = this.gameState.checkedCreateStateFromMove(move);
             } else { // enemy turn
                 var move = createMove(serverMove);
                 this.gameState = this.gameState.checkedCreateStateFromMove(move);
@@ -57,13 +57,13 @@ public class Client {
     }
 
     private static long createMove(Move serverMove) {
-        return BigInteger.valueOf(2).pow(serverMove.y).longValue() + serverMove.x;
+        return 1L << (serverMove.y * Configurations.FIELD_SIZE + serverMove.x);
     }
 
     private static Move createServerMove(int playerNumber, long move) {
         var serverMove = Utils.floorLog2(move);
-        var x = serverMove % 7;
-        var y = serverMove / 7;
+        var x = serverMove % Configurations.FIELD_SIZE;
+        var y = serverMove / Configurations.FIELD_SIZE;
         return new Move(playerNumber, x, y);
     }
 }
--- src/main/java/de/htw/aiforgames/Main.java
+++ src/main/java/de/htw/aiforgames/Main.java
@@ -26,27 +26,17 @@ public class Main {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        new Thread(() -> Server.runOnceAndReturnTheWinner(5)).start();
-        Thread.sleep(2000);
+        // new Thread(() -> Server.runOnceAndReturnTheWinner(5)).start();
+        // Thread.sleep(2000);
 
-        ClientWrapper decisionClient = new ClientWrapper(null, "Team 0", new DecisionRuleAlgorithm(7));
-        ClientWrapper randomClient0 = new ClientWrapper(null, "Team 1", new RandomAlgorithm());
-        ClientWrapper randomClient1 = new ClientWrapper(null, "Team 2", new RandomAlgorithm());
-        ClientWrapper randomClient2 = new ClientWrapper(null, "Team 3", new RandomAlgorithm());
+        ClientWrapper decisionClient = new ClientWrapper(null, "Team A", new DecisionRuleAlgorithm(7));
+        ClientWrapper randomClient0 = new ClientWrapper(null, "Team B", new RandomAlgorithm());
+        ClientWrapper randomClient1 = new ClientWrapper(null, "Team C", new RandomAlgorithm());
+        ClientWrapper randomClient2 = new ClientWrapper(null, "Team D", new RandomAlgorithm());
 
         new Thread(decisionClient).start();
-        System.out.println("client 0 started");
-        Thread.sleep(1000);
-
         new Thread(randomClient0).start();
-        System.out.println("client 1 started");
-        Thread.sleep(1000);
-
         new Thread(randomClient1).start();
-        System.out.println("client 2 started");
-        Thread.sleep(1000);
-
         new Thread(randomClient2).start();
-        System.out.println("client 3 started");
     }
 }
