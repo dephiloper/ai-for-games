7bfb44fafffc9cc945d32c2638126f77559835d5
Philipp <philippboensch@gmail.com>
2019-05-15 13:18:33 +0200

    finished client implementation

--- src/main/java/de/htw/aiforgames/Client.java
+++ src/main/java/de/htw/aiforgames/Client.java
@@ -1,9 +1,13 @@
+package de.htw.aiforgames;
+
+import de.htw.aiforgames.algorithm.DecisionAlgorithm;
 import lenz.htw.sawhian.Move;
 import lenz.htw.sawhian.net.NetworkClient;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class Client {
     private int playerNumber;
@@ -36,14 +40,14 @@ public class Client {
     }
 
     void run() {
-        while (true) {
+        while (!gameState.isGameOver()) {
             var serverMove = this.networkClient.receiveMove(); // blocking
             if (serverMove == null) { // our turn
                 var move = this.decisionAlgorithm.getNextMove(this.gameState);
-                serverMove = createServerMove(move);
+                serverMove = createServerMove(this.playerNumber, move);
                 this.networkClient.sendMove(serverMove);
-
-            } else {
+                this.gameState = this.gameState.checkedCreateStateFromMove(move);
+            } else { // enemy turn
                 var move = createMove(serverMove);
                 this.gameState = this.gameState.checkedCreateStateFromMove(move);
             }
@@ -51,13 +55,13 @@ public class Client {
     }
 
     private static long createMove(Move serverMove) {
-
+        return BigInteger.valueOf(2).pow(serverMove.y).longValue() + serverMove.x;
     }
 
     private static Move createServerMove(int playerNumber, long move) {
-        var hrMove = Utils.floorLog2(move);
-        var x = hrMove % 7;
-        var y = hrMove / 7;
+        var serverMove = Utils.floorLog2(move);
+        var x = serverMove % 7;
+        var y = serverMove / 7;
 
         return new Move(playerNumber, x, y);
     }
