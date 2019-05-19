427d04b4df17218df050af98c166acf02325dd3c
Bluemi <bruno.schilling@protonmail.ch>
2019-05-19 18:54:03 +0200

    added group.txt; added Tournament Client

---
+++ src/main/java/de/htw/aiforgames/TournamentClient.java
@@ -0,0 +1,15 @@
+package de.htw.aiforgames;
+
+import de.htw.aiforgames.algorithm.DecisionRuleAlgorithm;
+import de.htw.aiforgames.algorithm.Rater;
+
+public class TournamentClient {
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            Client client = Client.create(args[0], "Team Human", "/test1.png", new DecisionRuleAlgorithm(7, Rater.withLearned(), false));
+            client.execute();
+        } else {
+            System.out.println("usage: client <hostname>");
+        }
+    }
+}
